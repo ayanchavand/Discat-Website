cf669c285b5a8d4bb854ca050dd5a8f98d04e1e9
ayanchavand <ayan.chavand15@gmail.com>
2024-08-18 17:19:19 +0530

    i hate that I forget commiting every small thing but yeah, the cat button works now

--- DisCat-Frontend/src/Components/Body.jsx
+++ DisCat-Frontend/src/Components/Body.jsx
@@ -1,18 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios'
 
-export default function ReportBody(){
+export default function ReportBody({sendCat, sendDog}){
+    const [url, setUrl] = useState('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjvKVPWNACMZqeZEIKjjn4_ihfsK1y9jUjiw&s')
+    
+    const handleCatClick = async () =>{
+        const apiUrl = 'https://api.thecatapi.com/v1/images/search'
+        try {
+            const res = await axios.get(apiUrl)
+            console.log(res.data[0].url)
+            setUrl(res.data[0].url)
+        } catch(error){
+            console.log(error.message)
+        }
+    }
+
+    const handleDogClick = async () => {
+
+    }
+    
     return(
         <>  
-            <div className="flex justify-center p-10 ">
+            <div className="flex justify-center  w-full h-[600px] overflow-hidden p-10 ">
                 <img 
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjvKVPWNACMZqeZEIKjjn4_ihfsK1y9jUjiw&s"
-            c   lassName="max-w-full h-auto"></img>
+                src={url}
+                className="w-full max-w-[800px] max-h-[500px] object-contain"></img>
             </div>
-            <div class="flex space-x-7 p-6 justify-center">
-                <button class="bg-purple-600 text-white font-bold py-4 px-8 rounded-full">
+            <div className="flex space-x-7 p-6 justify-center">
+                <button 
+                onClick={handleCatClick}
+                className="bg-purple-600 text-white font-bold py-4 px-8 rounded-full">
                 Cats
                 </button>
-                <button class="bg-purple-600 text-white font-bold py-2 px-8 rounded-full">
+                <button 
+                onClick={handleDogClick}
+                className="bg-purple-600 text-white font-bold py-2 px-8 rounded-full">
                 Dogs
                 </button>
             </div>
--- DisCat-Frontend/src/Components/Footer.jsx
+++ DisCat-Frontend/src/Components/Footer.jsx
@@ -3,10 +3,9 @@ import React from "react";
 export default function Footer(){
     return(
         <>
-            
-            <footer className="fixed bottom-0 w-full">
+            <footer className="fixed bottom-2 w-full">
                 <hr></hr>
-                <h4 className="flex p-3 text-purpleWhite justify-center">Made with ❤️</h4>
+                <h4 className="flex p-4 text-purpleWhite justify-center">Made with ❤️</h4>
             </footer>
             
         </>
